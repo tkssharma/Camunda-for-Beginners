be6d99862e9442bb5f958cdce242e0112d7d8597
tkssharma <tarun.softengg@gmail.com>
2021-04-18 22:01:13 +0530

    updates

--- worker.js
+++ worker.js
@@ -1,41 +1,30 @@
-const { Client, logger } = require('camunda-external-task-client-js');
+const { Client, logger, Variables } = require('camunda-external-task-client-js');
 
 const config = {
-    baseUrl: 'http://localhost:8080/engine-rest',
+    baseUrl: 'http://192.168.29.254:8080/engine-rest',
     use: logger, 
     asyncResponseTimeout: 10000
 };
 
 const client = new Client(config);
 
-client.subscribe('save-database', async function ({ task, taskService }) {
-    const firstName = task.variables.get('firstName');
-    const email = task.variables.get('email');
-    console.log(task.variables);
-    // const age = task.variables.get('age');
-    console.log('save-db task')
+client.subscribe('check-action', async function ({ task, taskService }) {
+  try {
+  console.log('handling check-action...')
+  const localVariables = new Variables();
+  localVariables.set("type", 'holidayCheck');
+  await taskService.complete(task, localVariables);
+  console.log('done check-action')
+  }catch(err){
+    console.log(err);
+  }
 
-    await taskService.complete(task);
 });
-
-client.subscribe('send-email', async function ({ task, taskService }) {
-    const firstName = task.variables.get('firstName');
-    const email = task.variables.get('email');
-    // const age = task.variables.get('age');
-    console.log(task.variables);
-    console.log(`firstName: ${firstName}`);
-    console.log(`Email: ${email}`);
-    console.log('send-email task')
-    await taskService.complete(task);
-});
-
-client.subscribe('done', async function ({ task, taskService }) {
-    const firstName = task.variables.get('firstName');
-    const email = task.variables.get('email');
-    // const age = task.variables.get('age');
-    console.log(task.variables);
-    console.log(`firstName: ${firstName}`);
-    console.log(`Email: ${email}`);
-    console.log('send-email task')
-    await taskService.complete(task);
+client.subscribe('HolidayCheck', async function ({ task, taskService }) {
+  console.log('handling HolidayCheck')
+  console.log(task.variables.getAll());
+  const localVariables = new Variables();
+  localVariables.set("isHoliday", 'Yes');
+  await taskService.complete(task, localVariables);
+  console.log('done HolidayCheck')
 });
