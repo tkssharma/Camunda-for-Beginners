c26893effd1a374906eef7ad2bf87a01cdfb2308
tkssharma <tarun.softengg@gmail.com>
2021-04-19 01:31:55 +0530

    dmn tables added

--- worker.js
+++ worker.js
@@ -1,21 +1,50 @@
 const { Client, logger, Variables } = require('camunda-external-task-client-js');
 
 const config = {
-    baseUrl: 'http://192.168.29.254:8080/engine-rest',
-    use: logger, 
-    asyncResponseTimeout: 10000
+  baseUrl: 'http://192.168.29.254:8080/engine-rest',
+  use: logger,
+  asyncResponseTimeout: 10000
 };
 
 const client = new Client(config);
 
+client.subscribe('save-database', async function ({ task, taskService }) {
+  const firstName = task.variables.get('firstName');
+  const email = task.variables.get('email');
+  // const age = task.variables.get('age');
+  console.log('save-db task')
+
+  await taskService.complete(task);
+});
+
+client.subscribe('send-email', async function ({ task, taskService }) {
+  const firstName = task.variables.get('firstName');
+  const email = task.variables.get('email');
+  // const age = task.variables.get('age');
+  console.log(`firstName: ${firstName}`);
+  console.log(`Email: ${email}`);
+  console.log('send-email task')
+  await taskService.complete(task);
+});
+
+client.subscribe('done', async function ({ task, taskService }) {
+  const firstName = task.variables.get('firstName');
+  const email = task.variables.get('email');
+  // const age = task.variables.get('age');
+  console.log(`firstName: ${firstName}`);
+  console.log(`Email: ${email}`);
+  console.log('send-email task')
+  await taskService.complete(task);
+});
+
 client.subscribe('check-action', async function ({ task, taskService }) {
   try {
-  console.log('handling check-action...')
-  const localVariables = new Variables();
-  localVariables.set("type", 'holidayCheck');
-  await taskService.complete(task, localVariables);
-  console.log('done check-action')
-  }catch(err){
+    console.log('handling check-action...')
+    const localVariables = new Variables();
+    localVariables.set("type", 'holidayCheck');
+    await taskService.complete(task, localVariables);
+    console.log('done check-action')
+  } catch (err) {
     console.log(err);
   }
 
